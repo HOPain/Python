87d5f48cd9b786f452905c6ad4adb0f3be48fade
HOPain <hopain@disr.it>
2021-09-11 04:27:41 +0430

    Learning Basic

---
+++ 2. Python Basics/4. Operator Precedence.py
@@ -0,0 +1,11 @@
+# Operator Precedence = اولویت
+
+print(20 + 3 * 4)
+print(20 - 3 * 4)
+
+# Order = ترتیب اولویت ها
+
+# ()
+# **
+# * /
+# + -
---
+++ 2. Python Basics/5. Binary and Complex.py
@@ -0,0 +1,8 @@
+# Complex return full number
+print(complex(2.00005*2))
+
+# Bin return binary value of integer but python add "0b" add first of value
+print(bin(10))
+
+# Convert binary to integer
+print(int('0b1010', 2))
